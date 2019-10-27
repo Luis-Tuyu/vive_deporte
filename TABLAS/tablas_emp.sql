17ae348cf9cd7639d5724010a5bcd4d772cb93d2
Luis-Tuyu <e17081482@itmerida.edu.mx>
2019-10-27 16:43:32 -0600

    tabla empresas normalizada

--- TABLAS/tablas_emp.sql
+++ TABLAS/tablas_emp.sql
@@ -1,22 +1,23 @@
-/*subirlo al repositorio*/
-CREATE TABLE empresas
-(id_emp NUMBER(10) CONSTRAINT id_emp_pk PRIMARY KEY,
-nombre_emp VARCHAR2(100) CONSTRAINT nombre_emp_u UNIQUE,
-cel_emp NUMBER(10) CONSTRAINT cel_emp_u UNIQUE,
-direccion_emp VARCHAR2(100) CONSTRAINT direccion_emp_nn NOT NULL,
-representante_emp VARCHAR2(50) CONSTRAINT representante_emp_nn NOT NULL
+/*tablas normalizadas version 1.0*/
+CREATE TABLE Empresas
+(id_emp INT(5),
+ nombre_emp VARCHAR(80) NOT NULL UNIQUE,
+cel_emp VARCHAR(10) NOT NULL UNIQUE,
+direccion_emp VARCHAR(100) NOT NULL,
+representante_emp VARCHAR(50)NOT NULL,
+CONSTRAINT in_emp_pk PRIMARY KEY(id_emp)
 );
 
 CREATE TABLE administradores(
 id_admi NUMBER(3) CONSTRAINT id_admi_pk PRIMARY KEY, /*llave primaria nueva*/
- usuario_admi VARCHAR2(100) CONSTRAINT usuario_admi_u UNIQUE,
+ correo_admi VARCHAR(100) CONSTRAINT correo_admi_u UNIQUE  /*cambio de dato*/
  nombre_admi VARCHAR2(50) CONSTRAINT nombre_admi_nn NOT NULL,
  clave_admi VARCHAR2(100) CONSTRAINT clave_admi_nn NOT NULL,
  puesto_admi VARCHAR2(50) CONSTRAINT puesto_admi_nn NOT NULL,
  salario_Admi NUMBER(9,4),
  edad_admi NUMBER(3) CONSTRAINT edad_admi_nn NOT NULL,
  celular_admi NUMBER(10) CONSTRAINT celular_admi_u UNIQUE,
- correo_admi VARCHAR(100) CONSTRAINT correo_admi_u UNIQUE  
+ 
 );
 
 CREATE TABLE convocatorias(
@@ -32,37 +33,13 @@ no me lo reconoce con el plugin xd*/
 );
 
 CREATE TABLE categoria_varonil(
-modalidad_var VARCHAR2(100) CONSTRAINT modalidad_var_nn NOT NULL,
-cat1_var VARCHAR2(50),
-edad1_var_min NUMBER(2),
-edad1_var_max NUMBER(2),
-
-cat2_var VARCHAR2(50),
-edad2_var_min NUMBER(2),
-edad2_var_max NUMBER(2),
-
-cat3_var VARCHAR2(50),
-edad3_var_min NUMBER(2),
-edad3_var_max NUMBER(2),
-
-cat4_var VARCHAR2(50),
-edad4_var_min NUMBER(2),
-edad4_var_max NUMBER(2),
-
-cat5_var VARCHAR2(50),
-edad5_var_min NUMBER(2),
-edad5_var_max NUMBER(2),
-
-cat6_var VARCHAR2(50),
-edad6_var_min NUMBER(2),
-edad6_var_max NUMBER(2),
-
-cat7_var VARCHAR2(50),
-edad7_var_min NUMBER(2),
-edad7_var_max NUMBER(2),
-
-id_conv_var VARCHAR2(100),
-CONSTRAINT id_conv_var_fk FOREIGN KEY(id_conv_var)
+num_cv NUMBER(2) CONSTRAINT  num_cv_nn NOT NULL,
+modalidad_cv VARCHAR2(100) CONSTRAINT modalidad_cv_nn NOT NULL,
+nombre_cv VARCHAR2(50),
+edad_cv_min NUMBER(2),
+edad_cv_max NUMBER(2),
+id_conv VARCHAR2(100),
+CONSTRAINT id_conv_var_fk FOREIGN KEY(id_conv)
 REFERENCES convocatorias(id_conv)
 );
 
@@ -114,12 +91,14 @@ nacimiento_us DATE CONSTRAINT nacimiento_us_nn NOT NULL,
 CREATE TABLE contacto_emergencia(
 nombre_cm VARCHAR2(50),
 apellido_cm VARCHAR2(50),
-nombre_conv VARCHAR2(100),
+/*nombre_conv VARCHAR2(100),*/
 id_us NUMBER(7),
+cel_us VARCHAR(15),
 CONSTRAINT id_us_fk FOREIGN KEY(id_us)
 REFERENCES usuarios(id_us),
-CONSTRAINT nombre_conv_cm_fk FOREIGN KEY(nombre_conv)
+/*CONSTRAINT nombre_conv_cm_fk FOREIGN KEY(nombre_conv)
 REFERENCES convocatorias(nombre_conv)
+*/
 );
 
 CREATE TABLE inscripciones(
