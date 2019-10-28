489a33f992cecda5fbef3b983d0d928e477c9e82
Luis-Tuyu <e17081482@itmerida.edu.mx>
2019-10-27 18:17:50 -0600

    tablas usuarios N

--- TABLAS/tablas_emp.sql
+++ TABLAS/tablas_emp.sql
@@ -86,74 +86,22 @@ CREATE TABLE kits
  REFERENCES convocatorias(id_conv)
 );
 
-
-CREATE TABLE categoria_varonil(
-num_cv NUMBER(2) CONSTRAINT  num_cv_nn NOT NULL,
-modalidad_cv VARCHAR2(100) CONSTRAINT modalidad_cv_nn NOT NULL,
-nombre_cv VARCHAR2(50),
-edad_cv_min NUMBER(2),
-edad_cv_max NUMBER(2),
-id_conv VARCHAR2(100),
-CONSTRAINT id_conv_var_fk FOREIGN KEY(id_conv)
-REFERENCES convocatorias(id_conv)
+CREATE TABLE usuarios
+(id_us INT(5),
+ nombre_us VARCHAR(100) NOT NULL,
+ correo_us VARCHAR(100) NOT NULL UNIQUE,
+ cel_us VARCHAR(10) NOT NULL UNIQUE,
+ genero_us VARCHAR(1) NOT NULL,
+ fecha_nac_us DATE NOT NULL,
+ CONSTRAINT id_us_pk PRIMARY KEY(id_us)
 );
 
-CREATE TABLE categoria_femenil(
- modalidad_fem VARCHAR2(100) CONSTRAINT modalidad_fem_nn NOT NULL,   
-cat1_fem VARCHAR2(50),
-edad1_fem_min NUMBER(2),
-edad1_fem_max NUMBER(2),
-
-cat2_fem VARCHAR2(50),
-edad2_fem_min NUMBER(2),
-edad2_fem_max NUMBER(2),
-
-cat3_fem VARCHAR2(50),
-edad3_fem_min NUMBER(2),
-edad3_fem_max NUMBER(2),
-
-cat4_fem VARCHAR2(50),
-edad4_fem_min NUMBER(2),
-edad4_fem_max NUMBER(2),
-
-cat5_fem VARCHAR2(50),
-edad5_fem_min NUMBER(2),
-edad5_fem_max NUMBER(2),
-
-cat6_fem VARCHAR2(50),
-edad6_fem_min NUMBER(2),
-edad6_fem_max NUMBER(2),
-
-cat7_fem VARCHAR2(50),
-edad7_fem_min NUMBER(2),
-edad7_fem_max NUMBER(2),
-
-id_conv_fem VARCHAR2(100),
-CONSTRAINT id_conv_fem_fk FOREIGN KEY(id_conv_fem)
-REFERENCES convocatorias(id_conv)
-);
-
-CREATE TABLE usuarios(
-id_us NUMBER(7) CONSTRAINT id_us_pk PRIMARY KEY, 
-nombre_us VARCHAR2(50) CONSTRAINT nombre_us NOT NULL,
-apellido_us VARCHAR2(50) CONSTRAINT apellido_us NOT NULL, 
-correo_us VARCHAR2(80) CONSTRAINT correo_us_u UNIQUE,
-cel_us NUMBER(10)CONSTRAINT cel_us_u UNIQUE,
-genero_us CHAR CONSTRAINT genero_us_nn NOT NULL, 
-nacimiento_us DATE CONSTRAINT nacimiento_us_nn NOT NULL,
-);
-
-CREATE TABLE contacto_emergencia(
-nombre_cm VARCHAR2(50),
-apellido_cm VARCHAR2(50),
-/*nombre_conv VARCHAR2(100),*/
-id_us NUMBER(7),
-cel_us VARCHAR(15),
-CONSTRAINT id_us_fk FOREIGN KEY(id_us)
-REFERENCES usuarios(id_us),
-/*CONSTRAINT nombre_conv_cm_fk FOREIGN KEY(nombre_conv)
-REFERENCES convocatorias(nombre_conv)
-*/
+CREATE TABLE  contacto_emergencia
+(id_us INT(5),
+ nombre_cm VARCHAR(50) NOT NULL,
+ apellido_cm VARCHAR(50) NOT NULL,
+ cel_cm VARCHAR(10) NOT NULL,
+ CONSTRAINT id_us_pk PRIMARY KEY(id_us)
 );
 
 CREATE TABLE inscripciones(
@@ -171,12 +119,3 @@ CONSTRAINT id_conv_fk FOREIGN KEY(id_conv)
 REFERENCES convocatorias(id_conv)
 );
 
-CREATE TABLE kits(
-fecha_kits DATE CONSTRAINT fecha_kits_nn NOT NULL,
-hora_kits TIME,
-lugar_kits VARCHAR2(250),
-requisitos_kits VARCHAR2(250), /*Son los requisitos extra de la empresa*/
-nombre_conv VARCHAR2(100),
-CONSTRAINT id_conv_kits_fk FOREIGN KEY(id_conv)
-REFERENCES convocatorias(id_conv)
-);
