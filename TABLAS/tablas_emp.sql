42ea831830c605027f1a65122f6c26ee22501f49
Luis-Tuyu <e17081482@itmerida.edu.mx>
2019-10-27 18:07:35 -0600

    tablas convocatorias N

--- TABLAS/tablas_emp.sql
+++ TABLAS/tablas_emp.sql
@@ -31,18 +31,61 @@ CREATE TABLE Adminstradores_infoempresa
  CONSTRAINT id_admi_fk2 FOREIGN KEY(id_admi)
  REFERENCES Adminstradores_login(id_admi)
 );
-
-CREATE TABLE convocatorias(
-id_conv NUMBER(10) CONSTRAINT id_conv_pk PRIMARY KEY,/*cambio de llave primaria*/
-nombre_conv VARCHAR2(100) CONSTRAINT nombre_cov_u UNIQUE,
-lugar_conv VARCHAR2(100), 
-hora_conv TIME,
-fecha_conv DATE CONSTRAINT fecha_conv_u UNIQUE,
-costo_conv NUMBER(9,2) CONSTRAINT consto_conv_nn NOT NULL
-/*DEBEMOS LIGAR EL ID_EMP CON LOS EVENTOS*/
-/*imagen_ conv BFILE
-no me lo reconoce con el plugin xd*/
+/*igual se dividió por la normalizacion*/
+CREATE TABLE convocatorias
+(id_conv INT(5),
+ nombre_conv VARCHAR(100) NOT NULL,
+ costo_conv INT(10),
+ CONSTRAINT id_conv_pk PRIMARY KEY(id_conv)
 );
+
+CREATE TABLE convocatorias_precio
+(id_conv INT(5),
+ modalidad_cp VARCHAR(50) NOT NULL,
+ precio_cp INT(5) NOT NULL,
+ CONSTRAINT id_conv_pk FOREIGN KEY(id_conv)
+ REFERENCES convocatorias(id_conv)
+);
+
+CREATE TABLE convocatorias_infoevento
+(id_conv int(5),
+ lugar_conv VARCHAR(100) NOT NULL,
+ hora_conv TIME NOT NULL,
+ fecha_conv DATE NOT NULL,
+ CONSTRAINT id_conv_fk2 FOREIGN KEY(id_conv)
+ REFERENCES convocatorias(id_conv)
+);
+/*solamente es una tabla de relaciones con 2 llaves foraneas*/
+CREATE TABLE Empresas_evento(
+id_emp INT(5),
+id_conv INT(5),
+CONSTRAINT id_emp_fk FOREIGN KEY(id_emp)
+    REFERENCES Empresas(id_emp),
+CONSTRAINT id_emp_fk3 FOREIGN KEY(id_conv)
+    REFERENCES convocatorias(id_conv)
+);
+
+CREATE TABLE Ramas_eventos
+(id_conv INT(5),
+ rama_re VARCHAR(1),
+ modalidad_re VARCHAR(70),
+ categoria_re VARCHAR(70),
+ edad_re_min INT(3) NOT NULL,
+ edad_re_max INT(3) NOT NULL,
+ CONSTRAINT id_conv_fk4 FOREIGN KEY(id_conv)
+ REFERENCES convocatorias(id_conv),
+ CONSTRAINT combinacion_re_pk PRIMARY KEY(id_conv, rama_re, modalidad_re, categoria_re)
+);
+
+CREATE TABLE kits
+(id_conv INT(5),
+ fecha_kits DATE NOT NULL,
+ lugar_kits VARCHAR(100) NOT NULL,
+ requisitos_kits VARCHAR(200),
+ CONSTRAINT id_conv_fk5 FOREIGN KEY(id_conv)
+ REFERENCES convocatorias(id_conv)
+);
+
 
 CREATE TABLE categoria_varonil(
 num_cv NUMBER(2) CONSTRAINT  num_cv_nn NOT NULL,
