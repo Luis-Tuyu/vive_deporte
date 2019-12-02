40b30b708b16929bff7fdec37ef10dafa64a3f7c
LTUYU <franciscotuyupadron@gmail.com>
2019-12-02 02:17:30 -0600

    modificacion convocatorias

--- VD3.0/php/herramientas_metodos.php
+++ VD3.0/php/herramientas_metodos.php
@@ -446,8 +446,8 @@ $sql_sc="SELECT nombre_conv 'convocatorias', lugar_conv 'lugar', fecha_conv 'fec
 hora_conv 'hora' 
 FROM convocatorias c, convocatorias_infoevento cp 
 WHERE cp.id_conv = c.id_conv";
-$sql_sc2="SELECT modalidad_cp 'modalidad',precio_cp 'precio'
- FROM convocatorias_precio WHERE id_conv='$id_sc'";
+//$sql_sc2="SELECT modalidad_cp 'modalidad',precio_cp 'precio'
+ //FROM convocatorias_precio WHERE id_conv='$id_sc'";
         if($conexion_sc)
         { $query_sc=mysqli_query($conexion_sc, $sql_sc);
             while( $a_sc=mysqli_fetch_array($query_sc))
@@ -456,11 +456,11 @@ $sql_sc2="SELECT modalidad_cp 'modalidad',precio_cp 'precio'
                 echo 'lugar: '.$a_sc["lugar"].'<br>';
                 echo 'fecha: '.$a_sc["fecha"].'<br>';
                 echo 'hora: '.$a_sc["hora"].'<br>';
-                $query_sc2=mysqli_query($conexion_sc, $sql_sc2);
-            while( $a_sc2=mysqli_fetch_array($query_sc2))
+                //$query_sc2=mysqli_query($conexion_sc, $sql_sc2);
+            /*while( $a_sc2=mysqli_fetch_array($query_sc2))
             {   echo 'modalida: '.$a_sc2["modalidad"].'<br>';
                 echo 'precio: '.$a_sc2["precio"].'<br>';
-            }
+            }*/
             echo '<br><br><br><br><br>';
 
             }
